3f2efaecc3946d3d50f109ca79241431d75074a8
Harsh Patel <hi.arsh07@gmail.com>
2024-11-21 16:17:40 +0530

    all origins websocket

--- utils/socket.ts
+++ utils/socket.ts
@@ -9,10 +9,11 @@ let io: Server;
 export const initSocket = (server: HttpServer): void => {
     io = new Server(server, {
         cors: {
-            origin: [
-                process.env.REDIRECT_URL_FRONTEND || '',
-                process.env.URL_FRONTEND || ''
-            ].filter(url => url !== ''),
+            // origin: [
+            //     process.env.REDIRECT_URL_FRONTEND || '',
+            //     process.env.URL_FRONTEND || ''
+            // ].filter(url => url !== ''),
+            origin: '*',
             methods: ['GET', 'POST'],
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true,
